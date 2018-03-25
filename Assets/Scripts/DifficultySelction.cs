78e94357165746837c6302bfe6e245d79917ca3f
olgiordano <olgiordano@email.wm.edu>
2018-03-25 03:06:03 -0400

    New Edition

---
+++ Assets/Scripts/DifficultySelction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DifficultySelction : MonoBehaviour {
+
+    // Use this for initialization
+    public void StartGame ()
+    {
+        SceneManager.LoadScene("BasicScene");
+    }
+
+    // Update is called once per frame
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+}
